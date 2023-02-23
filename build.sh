298256fee79300323b29ae47451d762d495c07ba
blt__ <www.edav@gmail.com>
2023-02-23 17:04:07 +0400

    Make build script emit binaries with hyphens in names

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p out
 
 file=$(basename -- "$1")
-name=$(sed 's/\.asm$//' <<< $file)
+name=$(sed -e 's/\.asm$//' -e 's/_/-/' <<< $file)
 
 yasm -f elf64 -o out/$name.o -g dwarf2 $file 
 ld -o out/$name out/$name.o
